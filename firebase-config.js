88c6c29bbcfb927968568cb5ee13ca1175763a5c
cankan003301-ship-it <cankan003301@gmail.com>
2026-03-31 17:09:25 +0300

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -4,13 +4,14 @@
 // Firebase Console: https://console.firebase.google.com
 // ============================================================
 
-const FIREBASE_CONFIG = {
-  apiKey: "AIzaSyAbc123...",
-  authDomain: "benim-projem.firebaseapp.com",
-  projectId: "benim-projem",
-  storageBucket: "benim-projem.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "1:123456789:web:abc123..."
+const firebaseConfig = {
+  apiKey: "AIzaSyB_1ep3oiuwoz82NFEzIMwCzFTSnls1R9U",
+  authDomain: "isozaku.firebaseapp.com",
+  projectId: "isozaku",
+  storageBucket: "isozaku.firebasestorage.app",
+  messagingSenderId: "113708679267",
+  appId: "1:113708679267:web:05ffa080915a0b1d7c3856",
+  measurementId: "G-4B5GKVT9RZ"
 };
 
 // Firebase başlatma
